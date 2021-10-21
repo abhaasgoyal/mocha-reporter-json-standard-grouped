7d21365b75e14cbacd5433009b051728ab4cc41d
abhaasgoyal <goyalabhaas@gmail.com>
2021-10-21 15:47:11 +0530

    Add pass/fail tests for single root suite

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,35 +5,27 @@ import MochaGroupedReporter from "../src/index"
 
 const { Runner, Suite, Test } = Mocha
 
-const generateTest = (title : string, doneFn : any) => new Test(title, doneFn)
+const generateTest = (title: string, doneFn: any) => new Test(title, doneFn)
 
 describe('Grouped Mocha Test Reporter', () => {
 
-	let mocha : Mocha
+	let mocha: Mocha
 	let suite: Mocha.Suite
-	let subSuite: Mocha.Suite
-	let runner : Mocha.Runner
-	let mochaReporter : Mocha.Reporter
+	let subSuite1: Mocha.Suite // A root suite can have multiple grouped suites
+	let runner: Mocha.Runner
+	let mochaReporter: Mocha.Reporter
 
 
 	beforeEach(() => {
 		mocha = new Mocha({
 			reporter: MochaGroupedReporter
 		})
-		// mocha.reporter(MochaGroupedReporter, {
-		// 	reporterOptions: {
-		// 		quiet: true,
-		// 	}
-		// })
 
-		suite = new Suite('')
-		subSuite = new Suite("Mochawesome Suite")
+		suite = new Suite("Root Suite")
+		subSuite1 = new Suite("Group 1")
 		runner = new Runner(suite, false)
-		suite.addSuite(subSuite)
+		suite.addSuite(subSuite1)
 		mochaReporter = new (<any>mocha)._reporter(runner, {})
-
-		// Overwrite `uncaughtEnd` to prevent exit on failure
-		// runner.uncaughtEnd = () => { }
 
 	})
 
@@ -45,11 +37,61 @@ describe('Grouped Mocha Test Reporter', () => {
 	})
 
 	it('should have 1 passing test in 1 suite', function(done) {
-		const test = generateTest('passing test', () => {})
-		subSuite.addTest(test)
+		const test = generateTest('passing test', () => { })
+		subSuite1.addTest(test)
 		runner.run(failureCount => {
 			assert.strictEqual(failureCount, 0)
 			done()
 		})
 	})
+
+	it('should have 7 passing tests in 1 suite', function(done) {
+		const nTests = 7
+		for (let i: number = 0; i < nTests; i++) {
+			subSuite1.addTest(generateTest(`passing test ${i}`, () => { }))
+		}
+		runner.run(failureCount => {
+			assert.strictEqual(failureCount, 0)
+			done()
+		})
+	})
+
+	it('should have 1 passing test in 2 suites', function(done) {
+		// TODO: Correct subsuite definition
+		let subSuite2: Mocha.Suite = new Suite("Group 2")
+		suite.addSuite(subSuite2)
+		const test = generateTest('passing test', () => { })
+		subSuite1.addTest(test)
+		subSuite2.addTest(test)
+
+		runner.run(failureCount => {
+			assert.strictEqual(failureCount, 0)
+			done()
+		})
+	})
+
+	it('should have 1 failure in 1 suite', function(done) {
+		const error = { expected: 1, actual: 2 }
+		const test = generateTest('failing test', tDone => tDone(new assert.AssertionError(error)))
+		subSuite1.addTest(test)
+		runner.run(failureCount => {
+			assert.strictEqual(failureCount, 1)
+			done()
+		})
+	})
+
+	it('should have 7 failing tests in 1 suite', function(done) {
+		const nTests = 7
+		const error = { expected: 1, actual: 2 }
+		for (let i: number = 0; i < nTests; i++) {
+			subSuite1.addTest(generateTest(`failing test ${i}`,  tDone => tDone(new assert.AssertionError(error))))
+		}
+		runner.run(failureCount => {
+			assert.strictEqual(failureCount, 7)
+			done()
+		})
+	})
+
 })
+
+const genRandNat = (max: number) => Math.floor(Math.random() * max) + 1
