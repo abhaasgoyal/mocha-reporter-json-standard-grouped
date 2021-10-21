efc13bd68409aaf962c93cab114107b163770a28
abhaasgoyal <goyalabhaas@gmail.com>
2021-10-21 14:56:20 +0530

    Add testing infrastructure

--- src/index.ts
+++ src/index.ts
@@ -5,73 +5,80 @@
 import { Runner, reporters, Test, Suite } from 'mocha'
 import { CheckGeneralSchema } from "./check-general"
 
-module.exports = function (this: unknown, runner: Runner) {
-	const {
-		EVENT_RUN_END,
-		EVENT_SUITE_END,
-		EVENT_TEST_PASS,
-		EVENT_TEST_FAIL
-	} = Runner.constants
-	reporters.Base.call(this, runner)
+export default class MochaGroupedReporter extends reporters.Base {
 
-	const individualTests: Test[] = []
-	const rootSuites: { [key: string]: Test[] } = {}
+	constructor(runner: Runner) {
+		super(runner)
 
-	const results: CheckGeneralSchema = {
-		name: "Mocha unit tests",
-		description: "Mocha unit tests",
-		summary: "",
-		counts: { failure: 0, warning: 0, notice: 0 },
-		byFile: {},
-	}
-	runner.on(EVENT_SUITE_END, suite => {
-		if (suite.title !== "") {
-			const topMostSuite = getTopMostTitledSuite(suite)
-			const existingTopSuite = rootSuites[topMostSuite.title]
-			rootSuites[topMostSuite.title] = existingTopSuite !== undefined
-				? [...existingTopSuite, ...suite.tests]
-				: suite.tests
-		}
-	})
+		const {
+			EVENT_RUN_END,
+			EVENT_SUITE_END,
+			EVENT_TEST_PASS,
+			EVENT_TEST_FAIL
+		} = Runner.constants
+		reporters.Base.call(this, runner)
 
-	runner.on(EVENT_TEST_PASS, function (test) {
-		individualTests.push(test)
-	})
+		const individualTests: Test[] = []
+		const rootSuites: { [key: string]: Test[] } = {}
 
-	runner.on(EVENT_TEST_FAIL, function (test) {
-		individualTests.push(test)
-	})
-
-	runner.on(EVENT_RUN_END, function () {
-		const successfulSuiteNames = Object.keys(rootSuites).filter(sName => rootSuites[sName].every(t => t.state === "passed"))
-		const individualFailures = individualTests.filter(t => t.state !== "passed")
-
-		results.counts.notice = successfulSuiteNames.length
-		results.counts.failure = individualFailures.length
-		results.byFile["General"] = {
+		const results: CheckGeneralSchema = {
+			name: "Mocha unit tests",
+			description: "Mocha unit tests",
 			summary: "",
-			counts: { failure: results.counts.failure, warning: 0, notice: results.counts.notice },
-			details: [
-				...successfulSuiteNames.map((sName, i) => ({
-					Id: `success-${i}`,
-					title: sName,
-					message: `${rootSuites[sName].length} tests passed`,
-					category: "notice" as CheckGeneralSchema["byFile"]["details"]["details"][0]["category"],
-					rawDetails: rootSuites[sName].map(t => t.title).join("\n")
-				})),
-				...individualFailures.map((f, i) => ({
-					Id: `failure-${i}`,
-					title: f.title,
-					message: f.err?.message || "Error",
-					category: "failure" as CheckGeneralSchema["byFile"]["details"]["details"][0]["category"]
-				}))
-			]
-		} // as CheckGeneralSchema["byFile"]
+			counts: { failure: 0, warning: 0, notice: 0 },
+			byFile: {},
+		}
+		runner.on(EVENT_SUITE_END, suite => {
+			if (suite.title !== "") {
+				const topMostSuite = getTopMostTitledSuite(suite)
+				const existingTopSuite = rootSuites[topMostSuite.title]
+				rootSuites[topMostSuite.title] = existingTopSuite !== undefined
+					? [...existingTopSuite, ...suite.tests]
+					: suite.tests
+			}
+		})
 
-		console.log(JSON.stringify(results, null, 2))
-	})
+		runner.on(EVENT_TEST_PASS, function(test) {
+			individualTests.push(test)
+		})
+
+		runner.on(EVENT_TEST_FAIL, function(test) {
+			individualTests.push(test)
+		})
+
+		runner.on(EVENT_RUN_END, function() {
+			const successfulSuiteNames = Object.keys(rootSuites).filter(sName => rootSuites[sName].every(t => t.state === "passed"))
+			const individualFailures = individualTests.filter(t => t.state !== "passed")
+
+			results.counts.notice = successfulSuiteNames.length
+			results.counts.failure = individualFailures.length
+			results.byFile["General"] = {
+				summary: "",
+				counts: { failure: results.counts.failure, warning: 0, notice: results.counts.notice },
+				details: [
+					...successfulSuiteNames.map((sName, i) => ({
+						Id: `success-${i}`,
+						title: sName,
+						message: `${rootSuites[sName].length} tests passed`,
+						category: "notice" as CheckGeneralSchema["byFile"]["details"]["details"][0]["category"],
+						rawDetails: rootSuites[sName].map(t => t.title).join("\n")
+					})),
+					...individualFailures.map((f, i) => ({
+						Id: `failure-${i}`,
+						title: f.title,
+						message: f.err?.message || "Error",
+						category: "failure" as CheckGeneralSchema["byFile"]["details"]["details"][0]["category"]
+					}))
+				]
+			} // as CheckGeneralSchema["byFile"]
+
+			console.log(JSON.stringify(results, null, 2))
+		})
+	}
 }
 
 const getTopMostTitledSuite = (suite: Suite): Suite => {
 	return (suite.parent === undefined || suite.parent.title === "") ? suite : getTopMostTitledSuite(suite.parent)
 }
+
+module.exports = MochaGroupedReporter
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,55 @@
-import assert from "assert"
+import * as assert from "assert"
+// import * as childProcess from "child_process"
+import Mocha from "mocha"
+import MochaGroupedReporter from "../src/index"
 
-describe('sample()', function () {
-	it('should work', function () {
-		assert.ok(true)
+const { Runner, Suite, Test } = Mocha
+
+const generateTest = (title : string, doneFn : any) => new Test(title, doneFn)
+
+describe('Grouped Mocha Test Reporter', () => {
+
+	let mocha : Mocha
+	let suite: Mocha.Suite
+	let subSuite: Mocha.Suite
+	let runner : Mocha.Runner
+	let mochaReporter : Mocha.Reporter
+
+
+	beforeEach(() => {
+		mocha = new Mocha({
+			reporter: MochaGroupedReporter
+		})
+		// mocha.reporter(MochaGroupedReporter, {
+		// 	reporterOptions: {
+		// 		quiet: true,
+		// 	}
+		// })
+
+		suite = new Suite('')
+		subSuite = new Suite("Mochawesome Suite")
+		runner = new Runner(suite, false)
+		suite.addSuite(subSuite)
+		mochaReporter = new (<any>mocha)._reporter(runner, {})
+
+		// Overwrite `uncaughtEnd` to prevent exit on failure
+		// runner.uncaughtEnd = () => { }
+
 	})
 
+	it('should have 0 passing test in empty suite', function(done) {
+		runner.run(failureCount => {
+			assert.strictEqual(failureCount, 0)
+			done()
+		})
+	})
+
+	it('should have 1 passing test in 1 suite', function(done) {
+		const test = generateTest('passing test', () => {})
+		subSuite.addTest(test)
+		runner.run(failureCount => {
+			assert.strictEqual(failureCount, 0)
+			done()
+		})
+	})
 })
